acc63754acdfffa02f5336278afbc1f4807b4cb3
MachineLearningMike <fleetpro@gmail.com>
2023-05-31 17:17:13 +0000

    June 1

--- Notebooks/Transformer.py
+++ Notebooks/Transformer.py
@@ -1,8 +1,9 @@
 # Import 3rd-party frameworks.
 
+import numpy as np
+import tensorflow as tf
 from scipy import signal
 import matplotlib.pyplot as plt
-import numpy as np
 import time as tm
 from datetime import datetime, timedelta
 from matplotlib import pyplot as plt
@@ -268,14 +269,14 @@ num_heads = 8
 dropout_rate = 0.1
 
 
-transformer = Transformer(
-    num_layers=num_layers,
-    d_model=d_model,
-    num_heads=num_heads,
-    dff=dff,
-    input_vocab_size=tokenizers.pt.get_vocab_size().numpy(),
-    target_vocab_size=tokenizers.en.get_vocab_size().numpy(),
-    dropout_rate=dropout_rate)
+# transformer = Transformer(
+#     num_layers=num_layers,
+#     d_model=d_model,
+#     num_heads=num_heads,
+#     dff=dff,
+#     input_vocab_size=tokenizers.pt.get_vocab_size().numpy(),
+#     target_vocab_size=tokenizers.en.get_vocab_size().numpy(),
+#     dropout_rate=dropout_rate)
 
 
 class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
@@ -321,74 +322,74 @@ def masked_accuracy(label, pred):
   return tf.reduce_sum(match)/tf.reduce_sum(mask)
 
 
-transformer.compile(
-    loss=masked_loss,
-    optimizer=optimizer,
-    metrics=[masked_accuracy])
+# transformer.compile(
+#     loss=masked_loss,
+#     optimizer=optimizer,
+#     metrics=[masked_accuracy])
 
 
-transformer.fit(train_batches,
-                epochs=20,
-                validation_data=val_batches)
+# transformer.fit(train_batches,
+#                 epochs=20,
+#                 validation_data=val_batches)
 
 
-class Translator(tf.Module):
-  def __init__(self, tokenizers, transformer):
-    self.tokenizers = tokenizers
-    self.transformer = transformer
+# class Translator(tf.Module):
+#   def __init__(self, tokenizers, transformer):
+#     self.tokenizers = tokenizers
+#     self.transformer = transformer
 
-  def __call__(self, sentence, max_length=MAX_TOKENS):
-    # The input sentence is Portuguese, hence adding the `[START]` and `[END]` tokens.
-    assert isinstance(sentence, tf.Tensor)
-    if len(sentence.shape) == 0:
-      sentence = sentence[tf.newaxis]
+#   def __call__(self, sentence, max_length=MAX_TOKENS):
+#     # The input sentence is Portuguese, hence adding the `[START]` and `[END]` tokens.
+#     assert isinstance(sentence, tf.Tensor)
+#     if len(sentence.shape) == 0:
+#       sentence = sentence[tf.newaxis]
 
-    sentence = self.tokenizers.pt.tokenize(sentence).to_tensor()
+#     sentence = self.tokenizers.pt.tokenize(sentence).to_tensor()
 
-    encoder_input = sentence
+#     encoder_input = sentence
 
-    # As the output language is English, initialize the output with the
-    # English `[START]` token.
-    start_end = self.tokenizers.en.tokenize([''])[0]
-    start = start_end[0][tf.newaxis]
-    end = start_end[1][tf.newaxis]
+#     # As the output language is English, initialize the output with the
+#     # English `[START]` token.
+#     start_end = self.tokenizers.en.tokenize([''])[0]
+#     start = start_end[0][tf.newaxis]
+#     end = start_end[1][tf.newaxis]
 
-    # `tf.TensorArray` is required here (instead of a Python list), so that the
-    # dynamic-loop can be traced by `tf.function`.
-    output_array = tf.TensorArray(dtype=tf.int64, size=0, dynamic_size=True)
-    output_array = output_array.write(0, start)
+#     # `tf.TensorArray` is required here (instead of a Python list), so that the
+#     # dynamic-loop can be traced by `tf.function`.
+#     output_array = tf.TensorArray(dtype=tf.int64, size=0, dynamic_size=True)
+#     output_array = output_array.write(0, start)
 
-    for i in tf.range(max_length):
-      output = tf.transpose(output_array.stack())
-      predictions = self.transformer([encoder_input, output], training=False)
+#     for i in tf.range(max_length):
+#       output = tf.transpose(output_array.stack())
+#       predictions = self.transformer([encoder_input, output], training=False)
 
-      # Select the last token from the `seq_len` dimension.
-      predictions = predictions[:, -1:, :]  # Shape `(batch_size, 1, vocab_size)`.
+#       # Select the last token from the `seq_len` dimension.
+#       predictions = predictions[:, -1:, :]  # Shape `(batch_size, 1, vocab_size)`.
 
-      predicted_id = tf.argmax(predictions, axis=-1)
+#       predicted_id = tf.argmax(predictions, axis=-1)
 
-      # Concatenate the `predicted_id` to the output which is given to the
-      # decoder as its input.
-      output_array = output_array.write(i+1, predicted_id[0])
+#       # Concatenate the `predicted_id` to the output which is given to the
+#       # decoder as its input.
+#       output_array = output_array.write(i+1, predicted_id[0])
 
-      if predicted_id == end:
-        break
+#       if predicted_id == end:
+#         break
 
-    output = tf.transpose(output_array.stack())
-    # The output shape is `(1, tokens)`.
-    text = tokenizers.en.detokenize(output)[0]  # Shape: `()`.
+#     output = tf.transpose(output_array.stack())
+#     # The output shape is `(1, tokens)`.
+#     text = tokenizers.en.detokenize(output)[0]  # Shape: `()`.
 
-    tokens = tokenizers.en.lookup(output)[0]
+#     tokens = tokenizers.en.lookup(output)[0]
 
-    # `tf.function` prevents us from using the attention_weights that were
-    # calculated on the last iteration of the loop.
-    # So, recalculate them outside the loop.
-    self.transformer([encoder_input, output[:,:-1]], training=False)
-    attention_weights = self.transformer.decoder.last_attn_scores
+#     # `tf.function` prevents us from using the attention_weights that were
+#     # calculated on the last iteration of the loop.
+#     # So, recalculate them outside the loop.
+#     self.transformer([encoder_input, output[:,:-1]], training=False)
+#     attention_weights = self.transformer.decoder.last_attn_scores
 
-    return text, tokens, attention_weights
+#     return text, tokens, attention_weights
   
 
-  translator = Translator(tokenizers, transformer)
+#   translator = Translator(tokenizers, transformer)
 
   
